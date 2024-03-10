20008c0466397f35c1d8c5769f171a7d16e78ddf
Sergey <700520serj@mail.ru>
2024-03-10 17:22:10 +1000

    Создал структуру сайта и подключил библиотеки

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#myMenu',
+    autoScrolling:true,
+    scrollHorizontally: true,
+    recordHistory:false,
+    navigation:true,
+    showActiveTooltip:true,
+    credits:{enabled: false, label: 'Made with fullpage.js', position: 'right'},
+  });
